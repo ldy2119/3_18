68d3a86da5205e91ed6178591a7a12d0659df404
Quote <ldy2119@dgsw.hs.kr>
2019-03-18 15:34:00 +0900

    init

---
+++ src/main/java/kr/hs/dgsw/project_02_318/Project02318Application.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.project_02_318;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project02318Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project02318Application.class, args);
+    }
+
+}
